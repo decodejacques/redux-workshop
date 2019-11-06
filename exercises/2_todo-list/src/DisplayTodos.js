566daec3516d1d9c67839417068a54d56c10165d
decodejacques <31515171+decodejacques@users.noreply.github.com>
2019-11-06 13:09:08 -0500

    Update DisplayTodos.js

--- exercises/2_todo-list/src/DisplayTodos.js
+++ exercises/2_todo-list/src/DisplayTodos.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// This component doesn't use any data in the store! Fix this.
 class DisplayTodos extends Component {
   render = () => {
     return (<div> I need to get implemented </div>)
